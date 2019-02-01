400f7cebc9ee6d989bcf5f73d3c88ddc008a8cab
Daniel <daniel.sasu.etu@univ-lille.fr>
2019-02-01 23:58:49 +0100

    Complete model

--- project/server/models.py
+++ project/server/models.py
@@ -10,22 +10,26 @@ class Accident(db.Model):
     agg = db.Column('agg', db.Integer())
     int = db.Column('int', db.Integer())
     atm = db.Column('atm', db.Integer())
+    col = db.Column('col', db.Integer())
     adr = db.Column('adr', db.Text())
     comm = db.Column('comm', db.String(5))
+    gps = db.Column('gps', db.String(1))
     dep = db.Column('dep', db.String(3))
     lat = db.Column('lat', db.Float())
     long = db.Column('long', db.Float())
     date = db.Column('date', db.DateTime())
 
-    def __init__(self, num_acc, lum, agg, int, atm,
-                 adr, comm, dep, lat, long, date):
+    def __init__(self, num_acc, lum, agg, int, atm, col,
+                 adr, comm, gps, dep, lat, long, date):
         self.num_acc = num_acc
         self.lum = lum
         self.agg = agg
         self.int = int
         self.atm = atm
+        self.col = col
         self.adr = adr
         self.comm = comm
+        self.gps = gps
         self.dep = dep
         self.lat = lat
         self.long = long
@@ -37,6 +41,9 @@ class Accident(db.Model):
 
 class AccidentSchema(marshmallow.ModelSchema):
     class Meta:
+        fields = ('num_acc', 'lum', 'agg', 'int', 'atm', 'adr', 'col',
+                  'comm', 'gps', 'dep', 'lat', 'long', 'date')
+        ordered = True
         model = Accident
 
 
