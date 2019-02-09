882e1afbd105622e26989bd1321fca2c3aabd744
Daniel <daniel.sasu.etu@univ-lille.fr>
2019-02-09 16:22:04 +0100

    extend tests

--- project/tests/test_api.py
+++ project/tests/test_api.py
@@ -12,6 +12,34 @@ class TestApiBlueprint(BaseTestCase):
             self.assertIn(b"name", response.data)
             self.assertIn(b"version", response.data)
 
+    def test_accident_list(self):
+        with self.client:
+            response = self.client.get("/accident")
+            self.assertIn(b"\"type\": \"accident\"", response.data)
+            self.assertIn(b"usager", response.data)
+            self.assertIn(b"lieu", response.data)
+            self.assertIn(b"vehicule", response.data)
+
+    def test_accident_list(self):
+        with self.client:
+            response = self.client.get("/usager")
+            self.assertIn(b"\"type\": \"usager\"", response.data)
+            self.assertIn(b"relationships", response.data)
+            self.assertIn(b"accident", response.data)
+
+    def test_accident_list(self):
+        with self.client:
+            response = self.client.get("/lieu")
+            self.assertIn(b"\"type\": \"lieu\"", response.data)
+            self.assertIn(b"relationships", response.data)
+            self.assertIn(b"accident", response.data)
+
+    def test_accident_list(self):
+        with self.client:
+            response = self.client.get("/vehicule")
+            self.assertIn(b"\"type\": \"vehicule\"", response.data)
+            self.assertIn(b"relationships", response.data)
+            self.assertIn(b"accident", response.data)
 
 if __name__ == "__main__":
     unittest.main()
