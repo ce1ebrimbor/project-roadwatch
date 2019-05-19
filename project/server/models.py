32f7fef419fed38ba40b8ac2e57b52e575cbaad5
Daniel <daniel.sasu.etu@univ-lille.fr>
2019-05-19 16:28:36 +0200

    Default column value

--- project/server/models.py
+++ project/server/models.py
@@ -14,11 +14,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    admin = db.Column(db.Boolean())
-
-    def __init__(email, admin=False):
-        self.email = email
-        self.admin = admin
+    admin = db.Column(db.Boolean(), default=False)
 
     def get_id(self):
         return self.id
